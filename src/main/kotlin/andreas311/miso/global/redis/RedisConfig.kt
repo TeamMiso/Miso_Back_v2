4720fe2e78feeafe58a13b652ed563f9cb5255b3
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-05 15:11:55 +0900

    :memo: RedisConfig 구현

---
+++ src/main/kotlin/andreas311/miso/global/redis/RedisConfig.kt
@@ -0,0 +1,29 @@
+package andreas311.miso.global.redis
+
+import andreas311.miso.global.redis.properties.RedisProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableRedisRepositories
+class RedisConfig(
+    private val redisProperties: RedisProperties,
+) {
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory =
+        LettuceConnectionFactory(redisProperties.host, redisProperties.port)
+
+    @Bean
+    fun redisTemplate(): RedisTemplate<String, String> {
+        val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = StringRedisSerializer()
+        redisTemplate.setConnectionFactory(redisConnectionFactory())
+        return redisTemplate
+    }
+}
