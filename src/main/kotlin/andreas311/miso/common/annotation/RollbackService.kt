336f49a37ba23e2f28868c546b6121bcbcbe2aab
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-05 16:32:24 +0900

    :memo: RollbackService 어노테이션 구현

---
+++ src/main/kotlin/andreas311/miso/common/annotation/RollbackService.kt
@@ -0,0 +1,10 @@
+package andreas311.miso.common.annotation
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@Service
+@Transactional(rollbackFor = [Exception::class])
+annotation class RollbackService
