71cab0e83705bce04c4fda7456a66dbacb9fe5d7
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-11 15:12:51 +0900

    :memo: FcmNotificationPort 구현

---
+++ src/main/kotlin/andreas311/miso/domain/notification/application/port/output/FcmNotificationPort.kt
@@ -0,0 +1,9 @@
+package andreas311.miso.domain.notification.application.port.output
+
+import andreas311.miso.domain.notification.domain.NotificationAlarm
+
+interface FcmNotificationPort {
+    fun sendInquiryNotification(deviceToken: String, notificationAlarm: NotificationAlarm)
+
+    fun sendEnvironmentNotification(deviceTokens: List<String>, notificationAlarm: NotificationAlarm)
+}
