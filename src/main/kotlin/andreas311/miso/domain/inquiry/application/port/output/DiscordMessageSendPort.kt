1dc2c33ff500cc73fa8c7ec39d4f5438aed0e67c
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-12 17:26:22 +0900

    :memo: DiscordMessageSendPort 구현

---
+++ src/main/kotlin/andreas311/miso/domain/inquiry/application/port/output/DiscordMessageSendPort.kt
@@ -0,0 +1,9 @@
+package andreas311.miso.domain.inquiry.application.port.output
+
+interface DiscordMessageSendPort {
+    fun sendDiscordMessage(message: String)
+
+    fun toSingleDiscordMessage(string: String): String
+
+    fun createInquiryMessage(inquiryName: String): String
+}
