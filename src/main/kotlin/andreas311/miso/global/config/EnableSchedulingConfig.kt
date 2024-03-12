17036727ad7149534a8fc202b5442664fc158894
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-12 17:30:04 +0900

    :memo: EnableSchedulingConfig 구현

---
+++ src/main/kotlin/andreas311/miso/global/config/EnableSchedulingConfig.kt
@@ -0,0 +1,8 @@
+package andreas311.miso.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class EnableSchedulingConfig
