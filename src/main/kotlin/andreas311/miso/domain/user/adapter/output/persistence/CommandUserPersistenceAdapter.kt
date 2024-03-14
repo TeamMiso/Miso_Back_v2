89da16d401b5c75ce41be1bf8b4081350132e835
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-14 09:47:32 +0900

    :memo: CommandUserPersistenceAdapter 구현

---
+++ src/main/kotlin/andreas311/miso/domain/user/adapter/output/persistence/CommandUserPersistenceAdapter.kt
@@ -0,0 +1,23 @@
+package andreas311.miso.domain.user.adapter.output.persistence
+
+import andreas311.miso.domain.user.adapter.output.persistence.mapper.UserMapper
+import andreas311.miso.domain.user.adapter.output.persistence.repository.UserRepository
+import andreas311.miso.domain.user.application.port.output.CommandUserPort
+import andreas311.miso.domain.user.domain.User
+import org.springframework.stereotype.Component
+
+@Component
+class CommandUserPersistenceAdapter(
+    private val userMapper: UserMapper,
+    private val userRepository: UserRepository
+): CommandUserPort {
+    override fun saveUser(user: User): User {
+        val userEntity = userRepository.save(userMapper.toEntity(user))
+        return userMapper.toDomain(userEntity)!!
+    }
+
+    override fun deleteByEmail(email: String) {
+        val userEntity = userRepository.findByEmail(email)
+        return userRepository.delete(userEntity)
+    }
+}
