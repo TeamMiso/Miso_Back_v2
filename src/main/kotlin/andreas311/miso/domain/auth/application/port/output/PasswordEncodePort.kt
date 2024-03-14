f0fc1c0a2cbc51ef8036f25e23071f7e5effa0f5
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-14 09:51:14 +0900

    :memo: PasswordEncodePort 에 isPasswordMatch 함수 수정

--- src/main/kotlin/andreas311/miso/domain/auth/application/port/output/PasswordEncodePort.kt
+++ src/main/kotlin/andreas311/miso/domain/auth/application/port/output/PasswordEncodePort.kt
@@ -3,5 +3,5 @@ package andreas311.miso.domain.auth.application.port.output
 interface PasswordEncodePort {
     fun passwordEncode(password: String): String
 
-    fun isPasswordMatch(password: String, passwordCheck: String): Boolean
+    fun isPasswordMatch(password: String, passwordCheck: String)
 }
