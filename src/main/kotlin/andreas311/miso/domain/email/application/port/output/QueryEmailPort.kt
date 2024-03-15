b52fe5efcbea5aa03831c721539e73fe2aaeda77
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-15 09:04:23 +0900

    :memo: QueryEmailPort 에 findByRandomKeyOrNull 함수 추가

--- src/main/kotlin/andreas311/miso/domain/email/application/port/output/QueryEmailPort.kt
+++ src/main/kotlin/andreas311/miso/domain/email/application/port/output/QueryEmailPort.kt
@@ -5,4 +5,5 @@ import andreas311.miso.domain.email.domain.Email
 interface QueryEmailPort {
     fun findByEmailOrNull(email: String): Email?
     fun existsByEmail(email: String): Boolean
+    fun findByRandomKeyOrNull(randomKey: String): Email?
 }
