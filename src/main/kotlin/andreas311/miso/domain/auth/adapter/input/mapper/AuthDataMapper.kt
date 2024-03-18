dd8655f7b1cf42bcd30b18598c7dc08535f413b3
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-18 15:18:51 +0900

    :memo: AuthDataMapper 에 toDto, toResponse 함수 추가

--- src/main/kotlin/andreas311/miso/domain/auth/adapter/input/mapper/AuthDataMapper.kt
+++ src/main/kotlin/andreas311/miso/domain/auth/adapter/input/mapper/AuthDataMapper.kt
@@ -1,7 +1,11 @@
 package andreas311.miso.domain.auth.adapter.input.mapper
 
+import andreas311.miso.domain.auth.adapter.input.data.request.SignInRequest
 import andreas311.miso.domain.auth.adapter.input.data.request.SignUpRequest
+import andreas311.miso.domain.auth.adapter.input.data.response.TokenResponse
+import andreas311.miso.domain.auth.application.port.input.dto.SignInDto
 import andreas311.miso.domain.auth.application.port.input.dto.SignUpDto
+import andreas311.miso.domain.auth.application.port.output.dto.TokenDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,5 +15,19 @@ class AuthDataMapper {
             email = signUpRequest.email,
             password = signUpRequest.password,
             passwordCheck = signUpRequest.passwordCheck
+        )
+
+    infix fun toDto(signInRequest: SignInRequest): SignInDto =
+        SignInDto(
+            email = signInRequest.email,
+            password = signInRequest.password
+        )
+
+    infix fun toResponse(tokenDto: TokenDto): TokenResponse =
+        TokenResponse(
+            accessToken = tokenDto.accessToken,
+            refreshToken = tokenDto.refreshToken,
+            accessExp = tokenDto.accessExp,
+            refreshExp = tokenDto.refreshExp
         )
 }
