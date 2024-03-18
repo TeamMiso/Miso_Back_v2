a67957b2b995db8dd4acde042aeaa3526e3b7a4e
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-18 17:08:56 +0900

    :memo: RefreshTokenRepository 에 findByUserId 함수 추가

--- src/main/kotlin/andreas311/miso/domain/auth/adapter/output/persistence/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/andreas311/miso/domain/auth/adapter/output/persistence/repository/RefreshTokenRepository.kt
@@ -2,5 +2,8 @@ package andreas311.miso.domain.auth.adapter.output.persistence.repository
 
 import andreas311.miso.domain.auth.adapter.output.persistence.entity.RefreshTokenEntity
 import org.springframework.data.repository.CrudRepository
+import java.util.UUID
 
-interface RefreshTokenRepository: CrudRepository<RefreshTokenEntity, String>
+interface RefreshTokenRepository: CrudRepository<RefreshTokenEntity, String> {
+    fun findByUserId(userId: UUID): RefreshTokenEntity?
+}
