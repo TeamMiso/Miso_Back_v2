06bbcd0b1fbf39fcb2601adb442be5ee031d23d5
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-18 17:17:32 +0900

    :memo: RefreshTokenEntity 에 Indexed 어노테이션 추가

--- src/main/kotlin/andreas311/miso/domain/auth/adapter/output/persistence/entity/RefreshTokenEntity.kt
+++ src/main/kotlin/andreas311/miso/domain/auth/adapter/output/persistence/entity/RefreshTokenEntity.kt
@@ -2,12 +2,15 @@ package andreas311.miso.domain.auth.adapter.output.persistence.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
+import org.springframework.data.redis.core.index.Indexed
 import java.util.*
 
 @RedisHash(value = "refreshToken", timeToLive = 60L * 60 * 24 * 7)
 data class RefreshTokenEntity(
+    @Indexed
     val userId: UUID,
 
     @Id
+    @Indexed
     val refreshToken: String
 )
