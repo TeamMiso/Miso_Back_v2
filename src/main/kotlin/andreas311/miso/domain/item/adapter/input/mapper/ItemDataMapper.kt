77d3cd819e266ecaee0a6116cb68b44beea1d188
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-20 17:45:09 +0900

    :memo: ItemDataMapper 에 toDto 함수 추가

--- src/main/kotlin/andreas311/miso/domain/item/adapter/input/mapper/ItemDataMapper.kt
+++ src/main/kotlin/andreas311/miso/domain/item/adapter/input/mapper/ItemDataMapper.kt
@@ -1,7 +1,9 @@
 package andreas311.miso.domain.item.adapter.input.mapper
 
 import andreas311.miso.domain.item.adapter.input.data.request.CreateItemRequest
+import andreas311.miso.domain.item.adapter.input.data.request.EditItemRequest
 import andreas311.miso.domain.item.application.port.input.dto.CreateItemDto
+import andreas311.miso.domain.item.application.port.input.dto.EditItemDto
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,5 +14,13 @@ class ItemDataMapper {
             amount = createItemRequest.amount,
             name = createItemRequest.name,
             content = createItemRequest.content
+        )
+
+    infix fun toDto(editItemRequest: EditItemRequest): EditItemDto =
+        EditItemDto(
+            price = editItemRequest.price,
+            amount = editItemRequest.amount,
+            name = editItemRequest.name,
+            content = editItemRequest.content
         )
 }
