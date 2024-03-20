22ee146b0fcadbb22f24729f64aab70ee0892a6e
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-20 15:49:54 +0900

    :memo: CommandItemPersistenceAdapter 구현 및 saveItem 함수 추가

---
+++ src/main/kotlin/andreas311/miso/domain/item/adapter/output/persistence/CommandItemPersistenceAdapter.kt
@@ -0,0 +1,18 @@
+package andreas311.miso.domain.item.adapter.output.persistence
+
+import andreas311.miso.domain.item.adapter.output.persistence.mapper.ItemMapper
+import andreas311.miso.domain.item.adapter.output.persistence.repository.ItemRepository
+import andreas311.miso.domain.item.application.port.output.CommandItemPort
+import andreas311.miso.domain.item.domain.Item
+import org.springframework.stereotype.Component
+
+@Component
+class CommandItemPersistenceAdapter(
+    private val itemMapper: ItemMapper,
+    private val itemRepository: ItemRepository
+): CommandItemPort {
+    override fun saveItem(item: Item): Item {
+        val itemEntity = itemRepository.save(itemMapper toEntity item)
+        return itemMapper.toDomain(itemEntity)!!
+    }
+}
