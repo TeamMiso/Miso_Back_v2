68946a7e7940c23f871adc0ff7c725fb886b86ae
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-25 15:51:40 +0900

    :sparkles: 포인트 지급하기 api 구현

---
+++ src/main/kotlin/andreas311/miso/domain/user/application/service/GivePointService.kt
@@ -0,0 +1,18 @@
+package andreas311.miso.domain.user.application.service
+
+import andreas311.miso.common.annotation.RollbackService
+import andreas311.miso.domain.auth.application.port.output.UserSecurityPort
+import andreas311.miso.domain.user.application.port.input.GivePointUseCase
+import andreas311.miso.domain.user.application.port.output.CommandUserPort
+
+@RollbackService
+class GivePointService(
+    private val commandUserPort: CommandUserPort,
+    private val userSecurityPort: UserSecurityPort
+) : GivePointUseCase{
+    override fun execute() {
+        val user = userSecurityPort.currentUser()
+
+        commandUserPort.saveUser(user.addPoint(100))
+    }
+}
