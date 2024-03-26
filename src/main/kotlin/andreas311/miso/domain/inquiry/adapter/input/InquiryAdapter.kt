82345e25602b93c27901b7e4fa9912ec5912d00d
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-26 14:19:23 +0900

    :sparkles: InquiryAdapter 에 문의사항 리스트 api 관련 함수 추가

--- src/main/kotlin/andreas311/miso/domain/inquiry/adapter/input/InquiryAdapter.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/adapter/input/InquiryAdapter.kt
@@ -1,11 +1,14 @@
 package andreas311.miso.domain.inquiry.adapter.input
 
 import andreas311.miso.common.annotation.RequestController
-import andreas311.miso.domain.inquiry.adapter.input.data.WriteInquiryRequest
+import andreas311.miso.domain.inquiry.adapter.input.data.request.WriteInquiryRequest
+import andreas311.miso.domain.inquiry.adapter.input.data.response.ListInquiryResponse
 import andreas311.miso.domain.inquiry.adapter.input.mapper.InquiryDataMapper
+import andreas311.miso.domain.inquiry.application.port.input.ListInquiryUseCase
 import andreas311.miso.domain.inquiry.application.port.input.WriteInquiryUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
@@ -14,6 +17,7 @@ import javax.validation.Valid
 @RequestController("/inquiry")
 class InquiryAdapter(
     private val inquiryDataMapper: InquiryDataMapper,
+    private val listInquiryUseCase: ListInquiryUseCase,
     private val writeInquiryUseCase: WriteInquiryUseCase
 ) {
     @PostMapping
@@ -23,4 +27,10 @@ class InquiryAdapter(
     ): ResponseEntity<Void> =
         writeInquiryUseCase.execute(inquiryDataMapper toDto writeInquiryRequest, multipartFile)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @GetMapping
+    fun list(): ResponseEntity<ListInquiryResponse> =
+        listInquiryUseCase.execute()
+            .let { inquiryDataMapper.toResponse(it) }
+            .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 }
