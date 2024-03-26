d8ba59bf24afe6aa534c2bc493df0db50a138a68
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-26 17:53:02 +0900

    :memo: InquiryDataMapper 에 toResponse 함수 추가

--- src/main/kotlin/andreas311/miso/domain/inquiry/adapter/input/mapper/InquiryDataMapper.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/adapter/input/mapper/InquiryDataMapper.kt
@@ -1,8 +1,10 @@
 package andreas311.miso.domain.inquiry.adapter.input.mapper
 
 import andreas311.miso.domain.inquiry.adapter.input.data.request.WriteInquiryRequest
+import andreas311.miso.domain.inquiry.adapter.input.data.response.DetailInquiryResponse
 import andreas311.miso.domain.inquiry.adapter.input.data.response.InquiryResponse
 import andreas311.miso.domain.inquiry.adapter.input.data.response.ListInquiryResponse
+import andreas311.miso.domain.inquiry.application.port.input.dto.DetailInquiryDto
 import andreas311.miso.domain.inquiry.application.port.input.dto.ListInquiryDto
 import andreas311.miso.domain.inquiry.application.port.input.dto.WriteInquiryDto
 import org.springframework.stereotype.Component
@@ -13,6 +15,16 @@ class InquiryDataMapper {
         WriteInquiryDto(
             title = writeInquiryRequest.title,
             content = writeInquiryRequest.content
+        )
+
+    fun toResponse(detailInquiryDto: DetailInquiryDto): DetailInquiryResponse =
+        DetailInquiryResponse(
+            id = detailInquiryDto.id,
+            inquiryDate = detailInquiryDto.inquiryDate,
+            title = detailInquiryDto.title,
+            content = detailInquiryDto.content,
+            imageUrl = detailInquiryDto.imageUrl,
+            inquiryStatus = detailInquiryDto.inquiryStatus
         )
 
     fun toResponse(listInquiryDto: ListInquiryDto): ListInquiryResponse =
