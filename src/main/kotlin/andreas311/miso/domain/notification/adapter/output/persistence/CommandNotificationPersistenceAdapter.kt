02cb33ea7a8a5884317784a9b652a03376b3d503
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-27 20:16:22 +0900

    :memo: CommandNotificationPersistenceAdapter 구현

---
+++ src/main/kotlin/andreas311/miso/domain/notification/adapter/output/persistence/CommandNotificationPersistenceAdapter.kt
@@ -0,0 +1,18 @@
+package andreas311.miso.domain.notification.adapter.output.persistence
+
+import andreas311.miso.domain.notification.adapter.output.persistence.mapper.NotificationMapper
+import andreas311.miso.domain.notification.adapter.output.persistence.repository.NotificationRepository
+import andreas311.miso.domain.notification.application.port.output.CommandNotificationPort
+import andreas311.miso.domain.notification.domain.Notification
+import org.springframework.stereotype.Component
+
+@Component
+class CommandNotificationPersistenceAdapter(
+    private val notificationMapper: NotificationMapper,
+    private val notificationRepository: NotificationRepository
+) : CommandNotificationPort {
+    override fun saveNotification(notification: Notification): Notification {
+        val notificationEntity = notificationRepository.save(notificationMapper.toEntity(notification))
+        return notificationMapper.toDomain(notificationEntity)!!
+    }
+}
