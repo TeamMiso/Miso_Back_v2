d44b7ce3804dbe0603d7bb12a4133b86cfdc605d
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-27 20:17:22 +0900

    :memo: NotificationRepository 구현

---
+++ src/main/kotlin/andreas311/miso/domain/notification/adapter/output/persistence/repository/NotificationRepository.kt
@@ -0,0 +1,6 @@
+package andreas311.miso.domain.notification.adapter.output.persistence.repository
+
+import andreas311.miso.domain.notification.adapter.output.persistence.entity.NotificationEntity
+import org.springframework.data.repository.CrudRepository
+
+interface NotificationRepository : CrudRepository<NotificationEntity, Long>
