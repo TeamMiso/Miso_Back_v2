5fbf20952813e010ce2b6589435c890a0c03d2ec
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-28 14:16:06 +0900

    :memo: DetailNotificationDto 구현

---
+++ src/main/kotlin/andreas311/miso/domain/notification/application/port/input/dto/DetailNotificationDto.kt
@@ -0,0 +1,11 @@
+package andreas311.miso.domain.notification.application.port.input.dto
+
+import andreas311.miso.domain.notification.domain.Notification
+
+data class DetailNotificationDto(
+    val answer: String
+) {
+    constructor(notification: Notification) : this(
+        answer = notification.answer
+    )
+}
