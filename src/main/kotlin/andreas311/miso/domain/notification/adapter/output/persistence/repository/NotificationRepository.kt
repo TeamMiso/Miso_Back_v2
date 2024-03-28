11587ef041e1d7b61df3024c3ded011750f8bf8d
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-28 14:16:22 +0900

    :memo: NotificationRepository 에 findByInquiryId 함수 추가

--- src/main/kotlin/andreas311/miso/domain/notification/adapter/output/persistence/repository/NotificationRepository.kt
+++ src/main/kotlin/andreas311/miso/domain/notification/adapter/output/persistence/repository/NotificationRepository.kt
@@ -3,4 +3,6 @@ package andreas311.miso.domain.notification.adapter.output.persistence.repositor
 import andreas311.miso.domain.notification.adapter.output.persistence.entity.NotificationEntity
 import org.springframework.data.repository.CrudRepository
 
-interface NotificationRepository : CrudRepository<NotificationEntity, Long>
+interface NotificationRepository : CrudRepository<NotificationEntity, Long> {
+    fun findByInquiryId(id: Long): NotificationEntity?
+}
