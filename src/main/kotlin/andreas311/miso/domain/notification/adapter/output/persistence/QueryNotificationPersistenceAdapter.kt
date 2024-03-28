d81f8713edbd360ba70485c984217f3874dfc7ae
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-28 14:15:12 +0900

    :memo: QueryNotificationPersistenceAdapter 구현 및 findByInquiryIdOrNull 함수 작성

---
+++ src/main/kotlin/andreas311/miso/domain/notification/adapter/output/persistence/QueryNotificationPersistenceAdapter.kt
@@ -0,0 +1,18 @@
+package andreas311.miso.domain.notification.adapter.output.persistence
+
+import andreas311.miso.domain.notification.adapter.output.persistence.mapper.NotificationMapper
+import andreas311.miso.domain.notification.adapter.output.persistence.repository.NotificationRepository
+import andreas311.miso.domain.notification.application.port.output.QueryNotificationPort
+import andreas311.miso.domain.notification.domain.Notification
+import org.springframework.stereotype.Component
+
+@Component
+class QueryNotificationPersistenceAdapter(
+    private val notificationMapper: NotificationMapper,
+    private val notificationRepository: NotificationRepository
+) : QueryNotificationPort {
+    override fun findByInquiryIdOrNull(id: Long): Notification? {
+        val notificationEntity = notificationRepository.findByInquiryId(id)
+        return notificationMapper.toDomain(notificationEntity)
+    }
+}
