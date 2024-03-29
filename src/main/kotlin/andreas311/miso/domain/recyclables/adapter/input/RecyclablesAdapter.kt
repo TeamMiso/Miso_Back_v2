07c31d71cea2da6ea043fa8be90d622de6d547f9
uuuuuuuk <s22034@gsm.hs.kr>
2024-03-29 09:19:19 +0900

    :sparkles: RecyclablesAdapter 에 재활용품 방법 글 수정하기 api 관련 함수 추가

--- src/main/kotlin/andreas311/miso/domain/recyclables/adapter/input/RecyclablesAdapter.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/adapter/input/RecyclablesAdapter.kt
@@ -1,13 +1,16 @@
 package andreas311.miso.domain.recyclables.adapter.input
 
 import andreas311.miso.common.annotation.RequestController
-import andreas311.miso.domain.recyclables.adapter.input.data.CreateRecyclablesRequest
+import andreas311.miso.domain.recyclables.adapter.input.data.request.CreateRecyclablesRequest
+import andreas311.miso.domain.recyclables.adapter.input.data.request.EditRecyclablesRequest
 import andreas311.miso.domain.recyclables.adapter.input.mapper.RecyclablesDataMapper
 import andreas311.miso.domain.recyclables.application.port.input.CreateRecyclablesUseCase
 import andreas311.miso.domain.recyclables.application.port.input.DeleteRecyclablesUseCase
+import andreas311.miso.domain.recyclables.application.port.input.EditRecyclablesUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
@@ -17,6 +20,7 @@ import javax.validation.Valid
 @RequestController("/recyclables")
 class RecyclablesAdapter(
     private val recyclablesDataMapper: RecyclablesDataMapper,
+    private val editRecyclablesUseCase: EditRecyclablesUseCase,
     private val createRecyclablesUseCase: CreateRecyclablesUseCase,
     private val deleteRecyclablesUseCase: DeleteRecyclablesUseCase
 ) {
@@ -31,5 +35,14 @@ class RecyclablesAdapter(
     @DeleteMapping("/{id}")
     fun delete(@PathVariable id: Long): ResponseEntity<Void> =
         deleteRecyclablesUseCase.execute(id)
+            .let { ResponseEntity.status(HttpStatus.OK).build() }
+
+    @PatchMapping("/{id}")
+    fun edit(
+        @PathVariable id: Long,
+        @RequestPart(value = "file") multipartFile: MultipartFile?,
+        @RequestPart(value = "recyclables") @Valid editRecyclablesRequest: EditRecyclablesRequest
+    ): ResponseEntity<Void> =
+        editRecyclablesUseCase.execute(id, recyclablesDataMapper toDto editRecyclablesRequest, multipartFile)
             .let { ResponseEntity.status(HttpStatus.OK).build() }
 }
