0427fb8e2b6640c3af030033c1ffcc8916a3513f
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-01 11:52:42 +0900

    :memo: DetailRecyclablesDto 구현

---
+++ src/main/kotlin/andreas311/miso/domain/recyclables/application/port/input/dto/DetailRecyclablesDto.kt
@@ -0,0 +1,28 @@
+package andreas311.miso.domain.recyclables.application.port.input.dto
+
+import andreas311.miso.domain.recyclables.domain.Recyclables
+import andreas311.miso.domain.recyclables.domain.RecyclablesType
+
+data class DetailRecyclablesDto(
+    val id: Long,
+    val title: String,
+    val subTitle: String,
+    val recycleMethod: String,
+    val recycleTip: String,
+    val recycleCaution: String,
+    val imageUrl: String?,
+    val recyclablesType: RecyclablesType,
+    val recycleMark: String
+) {
+    constructor(recyclables: Recyclables) : this(
+        id = recyclables.id,
+        title = recyclables.title,
+        subTitle = recyclables.subTitle,
+        recycleMethod = recyclables.recycleMethod,
+        recycleTip = recyclables.recycleTip,
+        recycleCaution = recyclables.recycleCaution,
+        imageUrl = recyclables.imageUrl,
+        recyclablesType = recyclables.recyclablesType,
+        recycleMark = recyclables.recycleMark
+    )
+}
