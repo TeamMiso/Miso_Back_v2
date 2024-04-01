0726495000f6102a057bac3bf62773f53ed9cbcf
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-01 12:18:54 +0900

    :memo: RecyclablesRepository 에 findByRecyclablesType 함수 추가

--- src/main/kotlin/andreas311/miso/domain/recyclables/adapter/output/persistence/repository/RecyclablesRepository.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/adapter/output/persistence/repository/RecyclablesRepository.kt
@@ -1,7 +1,9 @@
 package andreas311.miso.domain.recyclables.adapter.output.persistence.repository
 
 import andreas311.miso.domain.recyclables.adapter.output.persistence.entity.RecyclablesEntity
+import andreas311.miso.domain.recyclables.domain.RecyclablesType
 import org.springframework.data.repository.CrudRepository
 
 interface RecyclablesRepository : CrudRepository<RecyclablesEntity, Long> {
+    fun findByRecyclablesType(recyclablesType: RecyclablesType): RecyclablesEntity?
 }
