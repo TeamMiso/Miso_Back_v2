c54a1c6d44e789493c110986397964c6eeb12c4e
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-08 17:27:17 +0900

    :memo: CreateEnvironmentDto 에 쉼표 제거

--- src/main/kotlin/andreas311/miso/domain/environment/application/port/input/dto/CreateEnvironmentDto.kt
+++ src/main/kotlin/andreas311/miso/domain/environment/application/port/input/dto/CreateEnvironmentDto.kt
@@ -2,5 +2,5 @@ package andreas311.miso.domain.environment.application.port.input.dto
 
 data class CreateEnvironmentDto(
     val title: String,
-    val content: String,
+    val content: String
 )
