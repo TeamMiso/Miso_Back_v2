5ae48721ea5468beed4372262dd15eba41ce085d
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-09 11:23:42 +0900

    :memo: CommandEnvironmentPort 에 deleteEnvironment 함수 추가

--- src/main/kotlin/andreas311/miso/domain/environment/application/port/output/CommandEnvironmentPort.kt
+++ src/main/kotlin/andreas311/miso/domain/environment/application/port/output/CommandEnvironmentPort.kt
@@ -4,4 +4,5 @@ import andreas311.miso.domain.environment.domain.Environment
 
 interface CommandEnvironmentPort {
     fun saveEnvironment(environment: Environment): Environment
+    fun deleteEnvironment(environment: Environment)
 }
