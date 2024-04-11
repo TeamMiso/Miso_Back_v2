486cbe92933a48c49dc77061c9ecaac220a5e2ff
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-11 11:59:42 +0900

    :memo: QueryEnvironmentPort 에 findAll 함수 추가

--- src/main/kotlin/andreas311/miso/domain/environment/application/port/output/QueryEnvironmentPort.kt
+++ src/main/kotlin/andreas311/miso/domain/environment/application/port/output/QueryEnvironmentPort.kt
@@ -4,4 +4,5 @@ import andreas311.miso.domain.environment.domain.Environment
 
 interface QueryEnvironmentPort {
     fun findByIdOrNull(id: Long): Environment?
+    fun findAll(): List<Environment>
 }
