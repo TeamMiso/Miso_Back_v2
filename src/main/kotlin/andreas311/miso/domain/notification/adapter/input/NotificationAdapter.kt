7298225b8c411eed11b34f5717b2477cc4c27d59
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-11 17:46:23 +0900

    :sparkles: NotificationAdapter 에 fcm 토큰 저장 api 관련 함수 추가

--- src/main/kotlin/andreas311/miso/domain/notification/adapter/input/NotificationAdapter.kt
+++ src/main/kotlin/andreas311/miso/domain/notification/adapter/input/NotificationAdapter.kt
@@ -4,16 +4,24 @@ import andreas311.miso.common.annotation.RequestController
 import andreas311.miso.domain.notification.adapter.input.data.DetailNotificationResponse
 import andreas311.miso.domain.notification.adapter.input.mapper.NotificationDataMapper
 import andreas311.miso.domain.notification.application.port.input.DetailNotificationUseCase
+import andreas311.miso.domain.notification.application.port.input.SaveDeviceTokenUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 
 @RequestController("/notification")
 class NotificationAdapter(
     private val notificationDataMapper: NotificationDataMapper,
-    private val detailNotificationUseCase: DetailNotificationUseCase
+    private val saveDeviceTokenUseCase: SaveDeviceTokenUseCase,
+    private val detailNotificationUseCase: DetailNotificationUseCase,
 ) {
+    @PostMapping("/save/{token}")
+    fun save(@PathVariable(name = "token") deviceToken: String): ResponseEntity<Void> =
+        saveDeviceTokenUseCase.execute(deviceToken)
+            .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
     @GetMapping("/{id}")
     fun detail(@PathVariable id: Long): ResponseEntity<DetailNotificationResponse> =
         detailNotificationUseCase.execute(id)
