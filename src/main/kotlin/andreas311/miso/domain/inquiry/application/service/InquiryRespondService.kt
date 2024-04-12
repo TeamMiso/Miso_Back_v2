6e8048ed76c06984f7128318ee59bddf34b035c0
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-12 09:36:03 +0900

    :sparkles: InquiryRespondService 에 문의 사항 답변 관련 푸시 알림 기능 적용

--- src/main/kotlin/andreas311/miso/domain/inquiry/application/service/InquiryRespondService.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/application/service/InquiryRespondService.kt
@@ -3,26 +3,32 @@ package andreas311.miso.domain.inquiry.application.service
 import andreas311.miso.common.annotation.RollbackService
 import andreas311.miso.domain.auth.application.port.output.UserSecurityPort
 import andreas311.miso.domain.inquiry.application.exception.InquiryNotFoundException
-import andreas311.miso.domain.inquiry.application.port.input.WriteInquiryRespondUseCase
+import andreas311.miso.domain.inquiry.application.port.input.InquiryRespondUseCase
 import andreas311.miso.domain.inquiry.application.port.input.dto.WriteInquiryRespondDto
 import andreas311.miso.domain.inquiry.application.port.output.CommandInquiryPort
 import andreas311.miso.domain.inquiry.application.port.output.QueryInquiryPort
 import andreas311.miso.domain.inquiry.domain.InquiryStatus
+import andreas311.miso.domain.notification.application.port.input.InquiryNotificationSendUseCase
 import andreas311.miso.domain.notification.application.port.output.CommandNotificationPort
+import andreas311.miso.domain.notification.application.port.output.QueryDeviceTokenPort
 import andreas311.miso.domain.notification.domain.Notification
 
 @RollbackService
-class WriteInquiryRespondService(
+class InquiryRespondService(
     private val userSecurityPort: UserSecurityPort,
     private val queryInquiryPort: QueryInquiryPort,
     private val commandInquiryPort: CommandInquiryPort,
-    private val commandNotificationPort: CommandNotificationPort
-) : WriteInquiryRespondUseCase {
+    private val queryDeviceTokenPort: QueryDeviceTokenPort,
+    private val commandNotificationPort: CommandNotificationPort,
+    private val inquiryNotificationSendUseCase: InquiryNotificationSendUseCase
+) : InquiryRespondUseCase {
     override fun execute(id: Long, writeInquiryRespondDto: WriteInquiryRespondDto) {
         val user = userSecurityPort.currentUser()
 
         val inquiry = queryInquiryPort.findByIdOrNull(id)
             ?: throw InquiryNotFoundException()
+
+        val deviceToken = queryDeviceTokenPort.findByUserIdOrNull(user.id)
 
         commandInquiryPort.saveInquiry(inquiry.updateInquiryStatus(InquiryStatus.COMPLETE))
 
@@ -34,5 +40,7 @@ class WriteInquiryRespondService(
                 inquiry = inquiry
             )
         )
+
+        deviceToken?.let { inquiryNotificationSendUseCase.execute(inquiry, deviceToken.deviceToken) }
     }
 }
