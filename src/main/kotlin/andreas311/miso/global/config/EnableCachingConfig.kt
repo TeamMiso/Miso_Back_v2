5dd9d35aea35039c2074faf2541fcba6054f469d
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-22 22:02:07 +0900

    :memo: EnableCachingConfig 구현

---
+++ src/main/kotlin/andreas311/miso/global/config/EnableCachingConfig.kt
@@ -0,0 +1,8 @@
+package andreas311.miso.global.config
+
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableCaching
+class EnableCachingConfig
