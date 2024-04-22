ea5c32f9c9234fe8675084e71cd4e06cdb8d8410
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-22 22:37:26 +0900

    :memo: DeleteItemService 에 write around 캐시 전략 적용

--- src/main/kotlin/andreas311/miso/domain/item/application/service/DeleteItemService.kt
+++ src/main/kotlin/andreas311/miso/domain/item/application/service/DeleteItemService.kt
@@ -5,12 +5,14 @@ import andreas311.miso.domain.item.application.exception.ItemNotFoundException
 import andreas311.miso.domain.item.application.port.input.DeleteItemUseCase
 import andreas311.miso.domain.item.application.port.output.CommandItemPort
 import andreas311.miso.domain.item.application.port.output.QueryItemPort
+import org.springframework.cache.annotation.CacheEvict
 
 @RollbackService
 class DeleteItemService(
     private val queryItemPort: QueryItemPort,
     private val commandItemPort: CommandItemPort
 ) : DeleteItemUseCase {
+    @CacheEvict(cacheNames = ["item"], key = "#id", cacheManager = "redisCacheManager")
     override fun execute(id: Long) {
         val itemEntity = queryItemPort.findByIdOrNull(id)
             ?: throw ItemNotFoundException()
