509f7e6f834c4699a8a335d54fbe98f8215a55ce
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-22 22:38:09 +0900

    :memo: GivePointService 에 write around 캐시 전략 적용

--- src/main/kotlin/andreas311/miso/domain/user/application/service/GivePointService.kt
+++ src/main/kotlin/andreas311/miso/domain/user/application/service/GivePointService.kt
@@ -4,12 +4,14 @@ import andreas311.miso.common.annotation.RollbackService
 import andreas311.miso.domain.auth.application.port.output.UserSecurityPort
 import andreas311.miso.domain.user.application.port.input.GivePointUseCase
 import andreas311.miso.domain.user.application.port.output.CommandUserPort
+import org.springframework.cache.annotation.CacheEvict
 
 @RollbackService
 class GivePointService(
     private val commandUserPort: CommandUserPort,
     private val userSecurityPort: UserSecurityPort
 ) : GivePointUseCase {
+    @CacheEvict(cacheNames = ["userPoint"], key = "'userPoint'", cacheManager = "redisCacheManager")
     override fun execute() {
         val user = userSecurityPort.currentUser()
 
