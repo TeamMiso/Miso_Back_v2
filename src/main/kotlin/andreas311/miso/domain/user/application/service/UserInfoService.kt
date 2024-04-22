2bdf7bab633173ed1fe57d1f0ef764b83e50737d
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-22 22:37:03 +0900

    :memo: UserInfoService 에 look aside 캐시 전략 적용

--- src/main/kotlin/andreas311/miso/domain/user/application/service/UserInfoService.kt
+++ src/main/kotlin/andreas311/miso/domain/user/application/service/UserInfoService.kt
@@ -4,11 +4,13 @@ import andreas311.miso.common.annotation.ReadOnlyRollbackService
 import andreas311.miso.domain.auth.application.port.output.UserSecurityPort
 import andreas311.miso.domain.user.application.port.input.UserInfoUseCase
 import andreas311.miso.domain.user.application.port.input.dto.UserInfoDto
+import org.springframework.cache.annotation.Cacheable
 
 @ReadOnlyRollbackService
 class UserInfoService(
     private val userSecurityPort: UserSecurityPort
 ) : UserInfoUseCase {
+    @Cacheable(cacheNames = ["user"], key = "'userInfo'", cacheManager = "redisCacheManager")
     override fun execute(): UserInfoDto {
         val user = userSecurityPort.currentUser()
 
