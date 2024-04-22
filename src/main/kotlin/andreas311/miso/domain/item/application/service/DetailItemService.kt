950557681e4b49bba4c590110882d6267a0614b7
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-22 22:36:11 +0900

    :memo: DetailItemService 에 look aside 캐시 전략 적용

--- src/main/kotlin/andreas311/miso/domain/item/application/service/DetailItemService.kt
+++ src/main/kotlin/andreas311/miso/domain/item/application/service/DetailItemService.kt
@@ -5,11 +5,13 @@ import andreas311.miso.domain.item.application.exception.ItemNotFoundException
 import andreas311.miso.domain.item.application.port.input.DetailItemUseCase
 import andreas311.miso.domain.item.application.port.input.dto.DetailItemDto
 import andreas311.miso.domain.item.application.port.output.QueryItemPort
+import org.springframework.cache.annotation.Cacheable
 
 @ReadOnlyRollbackService
 class DetailItemService(
     private val queryItemPort: QueryItemPort
 ) : DetailItemUseCase {
+    @Cacheable(cacheNames = ["item"], key = "#id", cacheManager = "redisCacheManager")
     override fun execute(id: Long): DetailItemDto {
         val item = queryItemPort.findByIdOrNull(id)
             ?: throw ItemNotFoundException()
