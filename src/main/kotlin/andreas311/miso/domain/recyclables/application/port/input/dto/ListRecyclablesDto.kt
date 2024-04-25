456f06cf6ef5dfeb63c9738cf638aaab512bf5d9
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-25 18:01:16 +0900

    :recycle: ListRecyclablesDto 에 constructor 를 통한 리스트 초기화

--- src/main/kotlin/andreas311/miso/domain/recyclables/application/port/input/dto/ListRecyclablesDto.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/application/port/input/dto/ListRecyclablesDto.kt
@@ -2,4 +2,6 @@ package andreas311.miso.domain.recyclables.application.port.input.dto
 
 data class ListRecyclablesDto(
     val recyclablesList: List<RecyclablesDto>
-)
+) {
+    constructor() : this(emptyList())
+}
