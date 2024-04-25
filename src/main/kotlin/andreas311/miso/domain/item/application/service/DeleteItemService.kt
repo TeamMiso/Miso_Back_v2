cb928e91ef5345d1665ead42017d2f2c48efdaa2
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-25 18:04:44 +0900

    :sparkles: DeleteItemService 에 Caching 어노테이션을 사용한 write around 캐시 전략 사용

--- src/main/kotlin/andreas311/miso/domain/item/application/service/DeleteItemService.kt
+++ src/main/kotlin/andreas311/miso/domain/item/application/service/DeleteItemService.kt
@@ -6,13 +6,17 @@ import andreas311.miso.domain.item.application.port.input.DeleteItemUseCase
 import andreas311.miso.domain.item.application.port.output.CommandItemPort
 import andreas311.miso.domain.item.application.port.output.QueryItemPort
 import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Caching
 
 @RollbackService
 class DeleteItemService(
     private val queryItemPort: QueryItemPort,
     private val commandItemPort: CommandItemPort
 ) : DeleteItemUseCase {
-    @CacheEvict(cacheNames = ["item"], key = "#id", cacheManager = "redisCacheManager")
+    @Caching(evict = [
+        CacheEvict(cacheNames = ["item"], key = "#id", cacheManager = "redisCacheManager"),
+        CacheEvict(cacheNames = ["itemList"], key = "'itemList'", cacheManager = "redisCacheManager")
+    ])
     override fun execute(id: Long) {
         val itemEntity = queryItemPort.findByIdOrNull(id)
             ?: throw ItemNotFoundException()
