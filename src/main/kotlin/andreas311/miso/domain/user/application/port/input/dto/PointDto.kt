ceecb371397b91335c3e61719c1b32960121d0ce
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-25 17:09:47 +0900

    :recycle: PointDto 의 필드를 직접 초기화에서 constructor 를 통한 보조 생성자 방식으로 변경

--- src/main/kotlin/andreas311/miso/domain/user/application/port/input/dto/PointDto.kt
+++ src/main/kotlin/andreas311/miso/domain/user/application/port/input/dto/PointDto.kt
@@ -3,9 +3,13 @@ package andreas311.miso.domain.user.application.port.input.dto
 import andreas311.miso.domain.user.domain.User
 
 data class PointDto(
-    val point: Int = 0
+    val point: Int
 ) {
     constructor(user: User) : this(
         point = user.point
+    )
+
+    constructor() : this(
+        point = 0
     )
 }
