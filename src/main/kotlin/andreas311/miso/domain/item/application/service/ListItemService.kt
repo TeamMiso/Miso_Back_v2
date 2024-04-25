f375a8aa8e0f5938eac9019e8977b90f1886644f
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-25 18:07:07 +0900

    :sparkles: ListItemService 에 리스트 look aside 캐시 전략 사용

--- src/main/kotlin/andreas311/miso/domain/item/application/service/ListItemService.kt
+++ src/main/kotlin/andreas311/miso/domain/item/application/service/ListItemService.kt
@@ -5,11 +5,13 @@ import andreas311.miso.domain.item.application.port.input.ListItemUseCase
 import andreas311.miso.domain.item.application.port.input.dto.ItemDto
 import andreas311.miso.domain.item.application.port.input.dto.ListItemDto
 import andreas311.miso.domain.item.application.port.output.QueryItemPort
+import org.springframework.cache.annotation.Cacheable
 
 @ReadOnlyRollbackService
 class ListItemService(
     private val queryItemPort: QueryItemPort
 ) : ListItemUseCase {
+    @Cacheable(cacheNames = ["itemList"], key = "'itemList'", cacheManager = "redisCacheManager")
     override fun execute(): ListItemDto {
         return ListItemDto(
             itemList = queryItemPort.findAll()
