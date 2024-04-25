6dbf27f2524fc6d3452c32151f835b2c81ab55fa
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-25 17:10:26 +0900

    :recycle: DetailRecyclablesDto 의 필드를 직접 초기화에서 constructor 를 통한 보조 생성자 방식으로 변경

--- src/main/kotlin/andreas311/miso/domain/recyclables/application/port/input/dto/DetailRecyclablesDto.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/application/port/input/dto/DetailRecyclablesDto.kt
@@ -4,15 +4,15 @@ import andreas311.miso.domain.recyclables.domain.Recyclables
 import andreas311.miso.domain.recyclables.domain.RecyclablesType
 
 data class DetailRecyclablesDto(
-    val id: Long = 0L,
-    val title: String = "",
-    val subTitle: String = "",
-    val recycleMethod: String = "",
-    val recycleTip: String = "",
-    val recycleCaution: String = "",
-    val imageUrl: String? = "",
-    val recyclablesType: RecyclablesType = RecyclablesType.BAG,
-    val recycleMark: String = ""
+    val id: Long,
+    val title: String,
+    val subTitle: String,
+    val recycleMethod: String,
+    val recycleTip: String,
+    val recycleCaution: String,
+    val imageUrl: String?,
+    val recyclablesType: RecyclablesType,
+    val recycleMark: String
 ) {
     constructor(recyclables: Recyclables) : this(
         id = recyclables.id,
@@ -24,5 +24,17 @@ data class DetailRecyclablesDto(
         imageUrl = recyclables.imageUrl,
         recyclablesType = recyclables.recyclablesType,
         recycleMark = recyclables.recycleMark
+    )
+
+    constructor() : this(
+        id = 0L,
+        title = "",
+        subTitle = "",
+        recycleMethod = "",
+        recycleTip = "",
+        recycleCaution = "",
+        imageUrl = null,
+        recyclablesType = RecyclablesType.BAG,
+        recycleMark = ""
     )
 }
