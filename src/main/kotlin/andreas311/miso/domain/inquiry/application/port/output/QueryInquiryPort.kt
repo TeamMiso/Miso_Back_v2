27b682c403289e8bd2d5293ee0ad48f9a2ad464c
uuuuuuuk <s22034@gsm.hs.kr>
2024-06-11 17:44:55 +0900

    :fire: QueryInquiryPort 에 findAllByUser 함수 삭제

--- src/main/kotlin/andreas311/miso/domain/inquiry/application/port/output/QueryInquiryPort.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/application/port/output/QueryInquiryPort.kt
@@ -7,7 +7,6 @@ import andreas311.miso.domain.user.domain.User
 interface QueryInquiryPort {
     fun findByIdOrNull(id: Long): Inquiry?
     fun findAll(): List<Inquiry>
-    fun findAllByUser(user: User): List<Inquiry>
     fun findAllByInquiryStatus(inquiryStatus: InquiryStatus): List<Inquiry>
-    fun findAllByUserAndInquiryStatus(user: User, inquiryStatus: InquiryStatus): List<Inquiry>
+    fun findAllByUserAndInquiryStatus(user: User, inquiryStatus: InquiryStatus?): List<Inquiry>
 }
